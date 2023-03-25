4e71061a3521edf959fbd8f25683d4beda6257ae
natashamos <natasha.moskaljov@gmail.com>
2023-03-25 16:25:20 +0000

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        <a href="https://github.com/natashamos/react-weather-app">
-          Open-source code
-        </a>{" "}
-        by Natasha Moskaljov
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          <a href="https://github.com/natashamos/react-weather-app">
+            Open-source code
+          </a>{" "}
+          by Natasha Moskaljov
+        </footer>
+      </div>
     </div>
   );
 }
